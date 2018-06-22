99122141f4418e62e0ea076468cc9d67973ac3fc
Daniel Rodriguez <danrodz15.dr@icloud.com>
2018-06-22 12:24:33 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,5 +2,17 @@
 
 def countdown(num)
   counter = num
-  
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1 
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num)
+  counter = num
+  while counter > 0
+    puts "#{counter} SECOND(S)"
+    counter -= 1
+  end
 end
